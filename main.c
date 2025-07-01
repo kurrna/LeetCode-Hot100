25c90c99f5b478e355c75bb9b855108d9078efa9
Kurna <kurna@foxmail.com>
2025-07-01 16:37:24 +0800

    finish groupAnagrams with violent answer

--- main.c
+++ main.c
@@ -1,31 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#include "leetcode.h"
-
-// 测试用例函数
-void test()
-{
-    printf("==== 测试两数之和 ====\n");
-    int nums[] = {2, 7, 11, 15};
-    int target = 9;
-    int returnSize;
-
-    int *result = twoSum(nums, 4, target, &returnSize);
-    if (result)
-    {
-        printf("结果: [%d, %d]\n", result[0], result[1]);
-        free(result); // 释放动态分配的内存
-    }
-    else
-    {
-        printf("未找到解\n");
-    }
-}
+#include "groupAnagrams"
 
 int main()
 {
-    // 运行所有测试用例
     test();
 
     return 0;
