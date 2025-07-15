de02cbf62781e4d119ad8a49773e3484b6768f10
Kurna <kurna@foxmail.com>
2025-07-15 09:59:42 +0800

    对称二叉树，迭代

--- src/isSymmetric.c
+++ src/isSymmetric.c
@@ -6,19 +6,41 @@
 #include <tree.h>
 
 // recursion
-bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
-    if (p == NULL || q == NULL) {
-        return p == q;
+// bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
+//     if (p == NULL || q == NULL) {
+//         return p == q;
+//     }
+//     return p->val == q->val && isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
+// }
+//
+// bool isSymmetric(struct TreeNode *root) {
+//     if (root == NULL) return true;
+//     return isSameTree(root->left, root->right);
+// }
+
+// iteration
+bool check(struct TreeNode *a, struct TreeNode *b) {
+    struct TreeNode **q = malloc(sizeof(struct TreeNode *) * 100);
+    int head = 0, tail = -1, size = 2;
+    q[++tail] = a, q[++tail] = b;
+    while (size > 0) {
+        a = q[head], head = (head + 1) % 99;
+        b = q[head], head = (head + 1) % 99;
+        size -= 2;
+        if (!a && !b) continue;
+        if (!a || !b || a->val != b->val) return false;
+        tail = (tail + 1) % 99, q[tail] = a->left;
+        tail = (tail + 1) % 99, q[tail] = b->right;
+        tail = (tail + 1) % 99, q[tail] = a->right;
+        tail = (tail + 1) % 99, q[tail] = b->left;
+        size += 4;
     }
-    return p->val == q->val && isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
+    return true;
 }
 
 bool isSymmetric(struct TreeNode *root) {
-    if (root == NULL) return true;
-    return isSameTree(root->left, root->right);
+    return check(root, root);
 }
-
-// iteration
 
 void test() {
     int arr[] = {1, 2, 2, 3, 4, 4, 3};
