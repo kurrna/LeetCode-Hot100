3970021371c396469ffbf0dab5aa2ed0df31fb7d
Kurna <kurna@foxmail.com>
2025-09-24 19:22:05 +0800

    杨辉三角

---
+++ src/generate(118).cpp
@@ -0,0 +1,33 @@
+//
+// Created by Kurna on 25-9-24.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ret(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            ret[i].resize(i + 1);
+            ret[i][0] = ret[i][i] = 1;
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            }
+        }
+        return ret;
+    }
+};
+
+int main() {
+    Solution sol;
+    auto res = sol.generate(5);
+    for (int i = 0; i < res.size(); ++i) {
+        for (int j = 0; j < res[i].size(); ++j) {
+            cout << res[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << endl;
+}
