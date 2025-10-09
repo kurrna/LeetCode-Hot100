6fd6cb74b0574ae0b85fd8fe73b36bb2396bdda1
Kurna <kurna@foxmail.com>
2025-10-09 20:41:57 +0800

    最大递增子序列，贪心算法+二分查找，维护一个数组d[i]，其表示长度为i的子序列的末尾元素的最小值

--- cpp/lengthOfLIS(300).cpp
+++ cpp/lengthOfLIS(300).cpp
@@ -22,6 +22,34 @@ public:
     }
 };
 
+class SolutionByBinarySearch {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        int len = 1, n = static_cast<int>(nums.size());
+        if (n == 0) return 0;
+        vector d(n + 1, 0);
+        d[len] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len, pos = 0;
+                while (l <= r) {
+                    int mid = (l + r) >>1;
+                    if (d[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
+};
+
 int main() {
     Solution sol;
     vector nums = {1,3,6,7,9,4,10,5,6};
