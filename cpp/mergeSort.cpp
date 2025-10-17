5460d2752ba93d7650b88a4df40b965d2892cba2
Kurna <kurna@foxmail.com>
2025-10-17 18:59:27 +0800

    增加归并排序的一种实现和单元测试

--- cpp/mergeSort.cpp
+++ cpp/mergeSort.cpp
@@ -3,6 +3,8 @@
 //
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cassert>
 using namespace std;
 
 // 时间复杂度O(nlogn)
@@ -46,10 +48,123 @@ void printArray(const vector<int> &arr) {
     cout << endl;
 }
 
+class Sort {
+public:
+    void Merge(vector<int> &arr, int l, int mid, int r) {
+        int i = l, j = mid + 1, k = l;
+        auto tmp = arr;
+        while (i <= mid && j <= r) {
+            if (arr[i] <= arr[j]) {
+                tmp[k++] = arr[i++];
+            } else {
+                tmp[k++] = arr[j++];
+            }
+        }
+        if (i > mid) {
+            while (k <= r) tmp[k++] = arr[j++];
+        } else {
+            while (k <= r) tmp[k++] = arr[i++];
+        }
+        arr = tmp;
+    }
+    void mergeSort(vector<int> &arr, int l, int r) {
+        if (l < r) {
+            int mid = (r - l) / 2 + l;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid + 1, r);
+            Merge(arr, l, mid, r);
+        }
+    }
+};
+
 int main() {
+    auto vectorsEqual = [](const vector<int>& a, const vector<int>& b) {
+        return a == b;
+    };
+
     MergeSort ms;
-    vector arr = {12, 11, 13, 5, 6, 7};
-    ms.mergeSort(arr, 0, arr.size() - 1);
-    printArray(arr);
+    Sort st;
+
+    // Empty array
+    {
+        vector<int> arr;
+        vector<int> expected = arr;
+        sort(expected.begin(), expected.end());
+        ms.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+        st.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+    }
+
+    // Single element
+    {
+        vector<int> arr = {42};
+        vector<int> expected = arr;
+        sort(expected.begin(), expected.end());
+        ms.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+        st.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+    }
+
+    // Already sorted
+    {
+        vector<int> arr = {1, 2, 3, 4, 5};
+        vector<int> expected = arr;
+        sort(expected.begin(), expected.end());
+        ms.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+        st.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+    }
+
+    // Reverse order
+    {
+        vector<int> arr = {5, 4, 3, 2, 1};
+        vector<int> expected = arr;
+        sort(expected.begin(), expected.end());
+        ms.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+        arr = {5,4,3,2,1};
+        st.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+    }
+
+    // With duplicates
+    {
+        vector<int> arr = {3, 1, 2, 3, 2, 1, 3};
+        vector<int> expected = arr;
+        sort(expected.begin(), expected.end());
+        ms.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+        arr = {3,1,2,3,2,1,3};
+        st.mergeSort(arr, 0, (int)arr.size() - 1);
+        assert(vectorsEqual(arr, expected));
+    }
+
+    // Multiple randomized cases
+    {
+        vector<vector<int>> cases = {
+            {7},
+            {2, 1},
+            {4, 5, 1, 3, 2},
+            {10, -1, 3, 3, 0, 5},
+            {5,5,5,5,5}
+        };
+        for (auto arr : cases) {
+            vector<int> expected = arr;
+            sort(expected.begin(), expected.end());
+
+            vector<int> a1 = arr;
+            ms.mergeSort(a1, 0, (int)a1.size() - 1);
+            assert(vectorsEqual(a1, expected));
+
+            vector<int> a2 = arr;
+            st.mergeSort(a2, 0, (int)a2.size() - 1);
+            assert(vectorsEqual(a2, expected));
+        }
+    }
+
+    cout << "All sorting tests passed." << endl;
     return 0;
 }
