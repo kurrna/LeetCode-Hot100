b3027652c1b7019a638c78569b20989ffa1a2ec0
Kurna <kurna@foxmail.com>
2026-03-19 23:33:01 +0800

    两数相加：在最前面加一个哑节点

---
+++ 2.两数相加.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=java
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = l1;
+        ListNode p1 = l1, p2 = l2, prev = dummy;
+        int carry = 0;
+        while (p1 != null || p2 != null) {
+            int x = (p1 != null) ? p1.val : 0;
+            int y = (p2 != null) ? p2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            if (p1 != null) {
+                p1.val = sum % 10;
+                prev = p1;
+                p1 = p1.next;
+            } else {
+                prev.next = new ListNode(sum % 10);
+                prev = prev.next;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            }
+        }
+        if (carry > 0) {
+            prev.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
