0eca0f5ee2d0088d631385660d4d7ebc874264d3
Kurna <kurna@foxmail.com>
2026-03-23 18:26:05 +0800

    合并两个有序链表

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+
+import java.util.*;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val > list2.val) return mergeTwoLists(list2, list1);
+        ListNode dummy = new ListNode(-101);
+        ListNode cur = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                cur.next = list2;
+                list2 = list2.next;
+            } else {
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            cur = cur.next;
+        }
+        if (list1 != null) {
+            cur.next = list1;
+        } else {
+            cur.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        var res = sol.mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.print(res.val + " ");
+            res = res.next;
+        }
+    }
+}
+// @lc code=end
+
