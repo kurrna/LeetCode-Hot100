6f6a496520de1ff192f7c4ad9fb0464589d61213
Kurna <kurna@foxmail.com>
2026-03-26 19:06:38 +0800

    搜索插入位置：二分查找

---
+++ 35.搜索插入位置.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        System.out.println(sol.searchInsert(new int[]{1, 3, 5, 6}, 5));
+    }
+}
+// @lc code=end
+
