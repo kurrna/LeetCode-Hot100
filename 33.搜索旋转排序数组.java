04698f0502d9114c51d8d332279c62391dbec700
Kurna <kurna@foxmail.com>
2026-03-26 17:03:34 +0800

    搜索旋转排序数组：二分查找

---
+++ 33.搜索旋转排序数组.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=java
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        int[] nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+        System.out.println(sol.search(nums, 0));
+    }
+}
+// @lc code=end
