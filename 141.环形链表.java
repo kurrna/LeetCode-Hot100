084dfc9a93dc148cc68a95b01c68d00d7e2be07c
Kurna <kurna@foxmail.com>
2026-04-21 13:59:12 +0800

    环形链表：快慢指针

---
+++ 141.环形链表.java
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        var sol = new Solution();
+        var head = new ListNode(1);
+        head.next = new ListNode(2);
+        // head.next.next = head;
+        System.out.println(sol.hasCycle(head));
+    }
+}
+// @lc code=end
+
